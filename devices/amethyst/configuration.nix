f007d51440196f4e435a6997792f27c14dc984ca
Velkee <89312879+Velkee@users.noreply.github.com>
2024-12-17 15:04:24 +0100

    Turn on auto optimise nix store

--- devices/amethyst/configuration.nix
+++ devices/amethyst/configuration.nix
@@ -69,6 +69,7 @@
 
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  nix.settings.auto-optimise-store = true;
 
   nixpkgs.config.allowUnfree = true;
   hardware.graphics.enable = true;
