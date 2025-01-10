dfe9fbc052718b5c8bd5bd2bd6683f8899613b4b
Velkee <89312879+Velkee@users.noreply.github.com>
2025-01-10 16:12:01 +0100

    Customize waybar more

--- home/modules/waybar/settings.nix
+++ home/modules/waybar/settings.nix
@@ -1,9 +1,29 @@
 {
   mainBar = {
-    layer = "top";
+    layer = "bottom";
     position = "top";
-    modules-left = [ "hyprland/workspaces" ];
+    modules-left = [ "hyprland/workspaces" "hyprland/window" ];
     modules-center = [ "clock" ];
-    modules-right = [ "tray"];
+    modules-right = [ "wireplumber" "tray" ];
+
+    "hyprland/workspaces" = {
+      all-outputs = true;
+    };
+
+    clock = {
+      tooltip-format = "<tt><small>{calendar}</small></tt>";
+      calendar = {
+        mode = "year";
+        mode-mon-col = 3;
+        week-pos = "left";
+        format = {
+          months = "<b>{}</b>";
+          days = "<b>{}</b>";
+          weeks = "<b>W{}</b>";
+          weekdays = "<b>{}</b>";
+          today = "<b><u>{}</u></b>";
+        };
+      };
+    };
   };
 }
