2db424211ae6ce007303ad386e992a657acd2f18
Velkee <89312879+Velkee@users.noreply.github.com>
2025-05-16 08:04:31 +0200

    Increaste track limit to improve shuffle

--- home/velkee/modules/spotify.nix
+++ home/velkee/modules/spotify.nix
@@ -2,8 +2,9 @@
   programs.spotify-player = {
     enable = true;
     settings = {
-      theme = "catppuccin-mocha";
       client_id = "22d5a4dc3aaf422d805807f8231985ac";
+      theme = "catppuccin-mocha";
+      tracks_playback_limit = 500;
       copy_command = {
         command = "wl-copy";
         args = [];
