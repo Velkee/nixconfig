8bcffae7f543c544edf68c78d59f130faae74628
Velkee <89312879+Velkee@users.noreply.github.com>
2025-06-09 00:17:25 +0200

    Change image sizing

--- home/velkee/modules/spotify.nix
+++ home/velkee/modules/spotify.nix
@@ -4,8 +4,8 @@
     settings = {
       client_id = "22d5a4dc3aaf422d805807f8231985ac";
       tracks_playback_limit = 500;
-      cover_img_width = 7;
-      cover_img_length = 16;
+      cover_img_width = 10;
+      cover_img_length = 20;
       copy_command = {
         command = "wl-copy";
         args = [];
