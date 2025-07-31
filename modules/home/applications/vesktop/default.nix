0cd580de49fbcb9617e91ec7f2f53ad266566651
Velkee <89312879+Velkee@users.noreply.github.com>
2025-07-31 11:23:11 +0200

    Disable vesktop plugin

--- modules/home/applications/vesktop/default.nix
+++ modules/home/applications/vesktop/default.nix
@@ -2,11 +2,13 @@
   lib,
   config,
   ...
-}: let
+}:
+let
   inherit (lib) mkEnableOption mkIf;
   inherit (config.modules) user;
   cfg = config.modules.applications.vesktop;
-in {
+in
+{
   options.modules.applications.vesktop = {
     enable = mkEnableOption "Enable Vesktop, the Discord client";
   };
@@ -15,23 +17,6 @@ in {
     home-manager.users.${user.name} = {
       programs.vesktop = {
         enable = true;
-        vencord.settings.plugins = {
-          TextReplace = {
-            enabled = true;
-            regexRules = [
-              {
-                find = "(?<!https?:\/\/.*)([Nn])([Aa])/g";
-                replace = "$1y$2";
-                onlyIfIncludes = "";
-              }
-              {
-                find = "(?<!https?:\/\/.*)NyA/g";
-                replace = "NYA";
-                onlyIfIncludes = "";
-              }
-            ];
-          };
-        };
       };
       xdg.desktopEntries = {
         vesktop = {
